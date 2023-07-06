121d005ca1e25852f62567db89e35fb13bc26db6
Hariom <hariompatel127@gmail.com>
2023-07-06 10:45:29 +0530

    package

--- src/Components/FileUpload.js
+++ src/Components/FileUpload.js
@@ -44,7 +44,7 @@ function FileUpload() {
     try {
       console.log("file", file);
       console.log("Auth:", authToken);
-      const response = await axios.post('/upload', formData, {
+      const response = await axios.post('https://8f0a-182-68-254-150.ngrok-free.app/upload', formData, {
         headers: {
           authorization: `${authToken}`,
         },
